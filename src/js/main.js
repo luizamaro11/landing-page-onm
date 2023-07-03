17fba4997f7d4c90f95ac11651893f0f8d38e4e3
luizamaro11 <luiz.amaro90@gmail.com>
2023-07-03 17:55:51 -0300

    pop-up web e mobile e inicio da página de obrigado

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,40 @@ faqs.forEach((faq) => {
         faq.classList.toggle("active")
     })
 })
+
+// Ação do CTA
+const ctas = document.querySelectorAll(".cta")
+const modal = document.querySelector("dialog")
+const btnClose = document.querySelector("dialog .close svg")
+let body = document.querySelector("body")
+
+ctas.forEach((cta) => {
+    cta.addEventListener("click", () => {
+        modal.showModal()
+
+        console.log(body)
+        body.style.filter = "blur(10px)"
+    })
+})
+
+btnClose.addEventListener("click", () => {
+    modal.close()
+
+    console.log(body)
+    body.style.filter = "none"
+})
+
+// Mascará input telefone
+
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+}
+  
+const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+}
